3e1a53d1f12ee7c1cb29a024a8364b2d960618eb
Usman Yahaya <yahayausman5@gmail.com>
2025-11-12 07:20:08 +0100

    inital commit task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,16 @@
+-- task_4.sql
+-- Script to print the full description of the table BOOKS
+-- Database name will be passed as an argument of the mysql command
+
+USE alx_book_store;
+
+SELECT 
+    TABLE_NAME,
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'BOOKS';
