bb8cac14de1fc65b07342a7dd5eb66e57bf510c7
Usman Yahaya <yahayausman5@gmail.com>
2025-11-12 07:13:29 +0100

    update on task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,4 +1,9 @@
 -- task_3.sql
 -- Script to list all tables in the given database (passed as argument)
 
+-- task_3.sql
+-- Script to list all tables in the alx_book_store database
+
+USE alx_book_store;
+
 SHOW TABLES;
