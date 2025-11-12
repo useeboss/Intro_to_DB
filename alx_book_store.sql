a5a79ef4317a8b2d58bd9d1c0bd5f6544361556f
Usman Yahaya <yahayausman5@gmail.com>
2025-11-12 02:22:58 +0100

    7th update

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -42,6 +42,6 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     ORDER_ID INT NOT NULL,
     book_id INT NOT NULL,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
-    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
