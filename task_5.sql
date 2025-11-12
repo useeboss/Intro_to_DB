f4ea10875771a19e25368e1b7ae8de260d47e8d0
Usman Yahaya <yahayausman5@gmail.com>
2025-11-12 07:28:02 +0100

    task_5.sql

---
+++ task_5.sql
@@ -0,0 +1,7 @@
+-- task_5.sql
+-- Script to insert a single row into CUSTOMERS table
+
+USE alx_book_store;
+
+INSERT INTO CUSTOMERS (CUSTOMER_ID, CUSTOMER_NAME, EMAIL)
+VALUES (1, 'Cole Baidoo', 'cbaidoo');
